563dbe81382a9264b9a01ec696219aa8ed45c66c
Vinovinoth2004 <148037192+Vinovinoth2004@users.noreply.github.com>
2023-10-16 11:12:04 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,7 @@
+examples = """
+Examples:
+  python main.py -h
+  python main.py -i <input_file> -o <output_file>
+  python main.py -i <input_file> -o <output_file> -l <language>
+  python main.py -i <input_file> -o <output_file> -l <language> -s <source_language>
+  python main.py -i <input_file>
